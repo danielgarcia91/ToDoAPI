ef91a0f19a6479e2138a96461606bcf34852bc8f
danielgarcia91 <dagarhern@gmail.com>
2018-12-26 15:30:42 -0600

    Updates to the classes and fixed bugs

--- ToDoApiTest/ToDoApiTest/Models/TodoContext.cs
+++ ToDoApiTest/ToDoApiTest/Models/TodoContext.cs
@@ -2,11 +2,12 @@
 
 namespace ToDoApiTest.Models
 {
-    public class TodoContext
+    public class TodoContext : DbContext 
     {
-        public TodoContext(DbContextOptions<TodoContext> options)
-            : base(options)
+        public TodoContext (DbContextOptions<TodoContext> options)
+                : base(options)
         {
+
         }
 
         public DbSet<TodoItem> TodoItems { get; set; }
--- ToDoApiTest/ToDoApiTest/Models/TodoItem.cs
+++ ToDoApiTest/ToDoApiTest/Models/TodoItem.cs
@@ -7,7 +7,7 @@ namespace ToDoApiTest.Models
 {
     public class TodoItem
     {
-        public long Id { get; set;} //This is the unique key in a relational db
+        public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
     }
