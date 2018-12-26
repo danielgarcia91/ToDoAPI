72409926d374acecca533fd805061523580b7804
danielgarcia91 <dagarhern@gmail.com>
2018-12-26 15:48:13 -0600

    Create method

--- ToDoApiTest/ToDoApiTest/Controllers/TodoController.cs
+++ ToDoApiTest/ToDoApiTest/Controllers/TodoController.cs
@@ -53,6 +53,15 @@ namespace ToDoApiTest.Controllers
             return todoItem;
         }
 
+
+        //POST: api.todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
     }
 
     /* Default Code
