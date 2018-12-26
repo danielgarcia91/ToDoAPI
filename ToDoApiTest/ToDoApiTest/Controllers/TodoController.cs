ed403f9ba9812c6d66b56829367214b20aaecc6d
danielgarcia91 <dagarhern@gmail.com>
2018-12-26 15:04:52 -0600

    The code defines an API controller without methods.

--- ToDoApiTest/ToDoApiTest/Controllers/TodoController.cs
+++ ToDoApiTest/ToDoApiTest/Controllers/TodoController.cs
@@ -15,6 +15,18 @@ namespace ToDoApiTest.Controllers
     public class TodoController : ControllerBase
     {
         private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+            if(_context.TodoItems.Count()==0)
+            {
+                //Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
     }
 
     /* Default Code
