6b982b4b08646fcce3fed281a04c267d920472cb
danielgarcia91 <dagarhern@gmail.com>
2018-12-26 15:01:23 -0600

    Adding controller

--- ToDoApiTest/ToDoApiTest/Startup.cs
+++ ToDoApiTest/ToDoApiTest/Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Unused usings removed
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using ToDoApiTest.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace ToDoApiTest
 {
@@ -27,8 +21,7 @@ namespace ToDoApiTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-                opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
