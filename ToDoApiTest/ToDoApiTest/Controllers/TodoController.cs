c0f05c9843ec6e7a479605aa9c0e3de0d1c8f53b
danielgarcia91 <dagarhern@gmail.com>
2018-12-26 16:33:58 -0600

    Delete Method has been added

--- ToDoApiTest/ToDoApiTest/Controllers/TodoController.cs
+++ ToDoApiTest/ToDoApiTest/Controllers/TodoController.cs
@@ -76,6 +76,24 @@ namespace ToDoApiTest.Controllers
             await _context.SaveChangesAsync();
             return NoContent();
         }
+
+
+        //DELETE: api/todo/5
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return todoItem;
+        }
+
     }
 
     /* Default Code
